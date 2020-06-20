138fd982f31c56dfe1698b991b50fb2a9c3198a1
Blvisse <blaisepke@gmail.com>
2020-06-20 17:49:40 +0300

    Creating a basic template and using url links

---
+++ Forms.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+#
+app = Flask(__name__)
+
+
+#
+#
+@app.route('/')
+def forms():
+    return render_template('forms.html')
+
+
+#
+#
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form.get('name')
+    return render_template('hello.html', name=name)
+
+
+#
+if __name__ == '__main__':
+    app.run()
